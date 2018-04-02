b4b8ab89a8ae691b4c5b7b0fa9c098eb2d427d63
Emehinola Idowu <hydeenoble39@gmail.com>
2018-04-02 21:44:08 +0100

    Started implementing the about section and added cool effects to the seconding heading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ExictingToursComponent } from './exicting-tours/exicting-tours.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ExictingToursComponent
   ],
   imports: [
     BrowserModule
